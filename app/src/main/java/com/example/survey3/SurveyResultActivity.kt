7aff8c24dd4387cf582bfb830e66e57a0ebc2a0f
bryanferries <bryan.ferries@go.minneapolis.edu>
2023-04-07 16:14:48 -0500

    why is it not displaying the correct value from either the view model OR the bundle method?  One gives me 100, the other gives me 0?

--- app/src/main/java/com/example/survey3/SurveyResultActivity.kt
+++ app/src/main/java/com/example/survey3/SurveyResultActivity.kt
@@ -3,8 +3,13 @@ package com.example.survey3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
 
 class SurveyResultActivity : AppCompatActivity() {
+
+    private val surveyViewModel: SurveyViewModel by lazy {
+        ViewModelProvider(this).get(SurveyViewModel::class.java)
+    }
 
     private lateinit var countFromMain: TextView
     private lateinit var countFromModel: TextView
@@ -14,13 +19,11 @@ class SurveyResultActivity : AppCompatActivity() {
         setContentView(R.layout.activity_survey_result)
 
         countFromMain = findViewById(R.id.count_from_main)
-        countFromModel = findViewById(R.id.count_textview)
+        countFromModel = findViewById(R.id.results_count)
 
         val countValue = intent.getIntExtra(EXTRA_COUNT_VALUE, 100)
 
         countFromMain.text = countValue.toString()
-        countFromModel.text =
-
-
+        countFromModel.text = surveyViewModel.getCount().toString()
     }
 }
